a70320201c77e3f50a3be7be33d2c084bd4ae7fc
RobinCombrink <robin.combrink@gmail.com>
2026-02-23 22:44:30 +0200

    Add WAV file output support

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+
+use hound;
 
 use cpal::{
-    Data, FromSample, InputCallbackInfo, OutputCallbackInfo, Sample, SampleFormat,
+    InputCallbackInfo,
     traits::{DeviceTrait, HostTrait, StreamTrait},
-};
-use ringbuf::{
-    HeapRb, SharedRb,
-    producer::DelegateProducer,
-    storage::Heap,
-    traits::{Consumer, Producer, Split},
-    wrap::caching::Caching,
 };
 
 use anyhow::Result;
@@ -20,19 +15,6 @@ fn main() -> Result<()> {
         .default_input_device()
         .expect(&format!("No available input device for host: {}", host.id()).to_string());
 
-    let output_device = host
-        .default_output_device()
-        .expect(&format!("No available output device for host: {}", host.id()).to_string());
-
-    let mut output_configs = output_device
-        .supported_output_configs()
-        .expect("error while querying output configs");
-
-    let output_config = output_configs
-        .next()
-        .expect("no supported output config?!")
-        .with_max_sample_rate();
-
     let mut input_configs = input_device
         .supported_input_configs()
         .expect("error while querying configs");
@@ -42,93 +24,116 @@ fn main() -> Result<()> {
         .expect("no supported config?!")
         .with_max_sample_rate();
 
-    let latency_frames = (3_000.0 / 1_000.0) * input_config.sample_rate() as f32;
-    let latency_samples = latency_frames as usize * input_config.channels() as usize;
-
-    let ring = HeapRb::<u8>::new(latency_samples * 2);
-    let (mut producer, consumer) = ring.split();
-
-    for _ in 0..latency_samples {
-        producer.try_push(0).unwrap();
-    }
-
-    let input_callback = create_input_callback(producer);
-
-    let output_callback = create_output_callback(consumer, 0);
-
-    println!(
-        "Input Configs: {:#?}",
-        input_configs
-            .map(|config| config.sample_format().clone())
-            .collect::<Vec<_>>()
-    );
-
     println!("Input config: {:#?}", input_config);
-    println!("Output config: {:#?}", output_config);
-
     println!("Input device: {}", input_device.description()?);
 
-    let input_stream =
-        input_device.build_input_stream(&input_config.into(), input_callback, err_fn, None)?;
-    let output_stream =
-        output_device.build_output_stream(&output_config.into(), output_callback, err_fn, None)?;
+    // Remove output file if it exists
+    let output_path = "output.wav";
+    let _ = std::fs::remove_file(output_path);
 
-    input_stream.play();
-    output_stream.play();
+    let spec = hound::WavSpec {
+        channels: input_config.channels(),
+        sample_rate: input_config.sample_rate(),
+        bits_per_sample: match input_config.sample_format() {
+            cpal::SampleFormat::I16 => 16,
+            cpal::SampleFormat::U16 => 16,
+            cpal::SampleFormat::U8 => 16, // We'll convert to i16
+            cpal::SampleFormat::F32 => 32,
+            _ => panic!("Unsupported sample format for WAV"),
+        },
+        sample_format: match input_config.sample_format() {
+            cpal::SampleFormat::I16 => hound::SampleFormat::Int,
+            cpal::SampleFormat::U16 => hound::SampleFormat::Int,
+            cpal::SampleFormat::U8 => hound::SampleFormat::Int,
+            cpal::SampleFormat::F32 => hound::SampleFormat::Float,
+            _ => panic!("Unsupported sample format for WAV"),
+        },
+    };
 
-    println!("Playing for some seconds... ");
+    let writer = Arc::new(Mutex::new(hound::WavWriter::create(output_path, spec)?));
+    let sample_format = input_config.sample_format();
+    let config = input_config.into();
+    let writer_clone = writer.clone();
+
+    let input_stream = match sample_format {
+        cpal::SampleFormat::U8 => input_device.build_input_stream(
+            &config,
+            move |data: &[u8], _: &InputCallbackInfo| {
+                let mut writer = writer_clone.lock().unwrap();
+                for &sample in data {
+                    // Convert u8 to i16 (centered at 0)
+                    let s = (sample as i16 - 128) << 8;
+                    let _ = writer.write_sample(s);
+                }
+            },
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::I16 => input_device.build_input_stream(
+            &config,
+            move |data: &[i16], _: &InputCallbackInfo| {
+                let mut writer = writer_clone.lock().unwrap();
+                for &sample in data {
+                    let _ = writer.write_sample(sample);
+                }
+            },
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::U16 => input_device.build_input_stream(
+            &config,
+            move |data: &[u16], _: &InputCallbackInfo| {
+                let mut writer = writer_clone.lock().unwrap();
+                for &sample in data {
+                    // Convert u16 to i16 (centered at 0)
+                    let s = (sample as i32 - 32768) as i16;
+                    let _ = writer.write_sample(s);
+                }
+            },
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::F32 => input_device.build_input_stream(
+            &config,
+            move |data: &[f32], _: &InputCallbackInfo| {
+                let mut writer = writer_clone.lock().unwrap();
+                for &sample in data {
+                    let _ = writer.write_sample(sample);
+                }
+            },
+            err_fn,
+            None,
+        )?,
+        cpal::SampleFormat::I8 => todo!(),
+        cpal::SampleFormat::I24 => todo!(),
+        cpal::SampleFormat::I32 => todo!(),
+        cpal::SampleFormat::I64 => todo!(),
+        cpal::SampleFormat::U24 => todo!(),
+        cpal::SampleFormat::U32 => todo!(),
+        cpal::SampleFormat::U64 => todo!(),
+        cpal::SampleFormat::F64 => todo!(),
+        _ => todo!(),
+    };
+
+    let _ = input_stream.play();
+
+    println!("Recording for 5 seconds...");
     std::thread::sleep(std::time::Duration::from_secs(5));
     drop(input_stream);
-    drop(output_stream);
-    println!("Done!");
+    let _ = Arc::try_unwrap(writer).map(|w| w.into_inner().unwrap().finalize());
+    println!("Done! Audio written to output.wav");
+
+    #[cfg(target_os = "windows")]
+    {
+        use std::process::Command;
+        let _ = Command::new("powershell")
+            .args([
+                "-c",
+                "(New-Object Media.SoundPlayer 'output.wav').PlaySync()",
+            ])
+            .status();
+    }
     Ok(())
-}
-
-fn create_input_callback<T, P>(
-    mut producer: P,
-) -> impl FnMut(&[T], &InputCallbackInfo) + Send + 'static
-where
-    T: Copy + Send + 'static,
-    P: Producer<Item = T> + Send + 'static,
-{
-    move |data: &[T], _: &InputCallbackInfo| {
-        let mut output_fell_behind = false;
-
-        for &sample in data {
-            if producer.try_push(sample).is_err() {
-                output_fell_behind = true;
-            }
-        }
-
-        if output_fell_behind {
-            eprintln!("output stream fell behind: try increasing latency");
-        }
-    }
-}
-
-fn create_output_callback<T, P>(
-    mut consumer: P,
-    default_sample: T,
-) -> impl FnMut(&mut [T], &OutputCallbackInfo) + Send + 'static
-where
-    T: Copy + Send + 'static,
-    P: Consumer<Item = T> + Send + 'static,
-{
-    move |data: &mut [T], _: &OutputCallbackInfo| {
-        let mut input_fell_behind = false;
-        for sample in data {
-            *sample = match consumer.try_pop() {
-                Some(s) => s,
-                None => {
-                    input_fell_behind = true;
-                    default_sample
-                }
-            };
-        }
-        if input_fell_behind {
-            eprintln!("input stream fell behind: try increasing latency");
-        }
-    }
 }
 
 fn err_fn(err: cpal::StreamError) {
