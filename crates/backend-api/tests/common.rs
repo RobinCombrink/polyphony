489bfe3203de966d5c448692fdd2fe2f4fda8a60
RobinCombrink <robin.combrink@gmail.com>
2026-03-06 21:45:29 +0200

    tests: prevent common.rs from being executed as a test

--- crates/backend-api/tests/common.rs
+++
@@ -1,4 +0,0 @@
-#[path = "common/bdd_support.rs"]
-pub(crate) mod bdd_support;
-#[path = "common/entity_seeder.rs"]
-pub(crate) mod entity_seeder;
---
+++ crates/backend-api/tests/common/mod.rs
@@ -0,0 +1,2 @@
+pub(crate) mod bdd_support;
+pub(crate) mod entity_seeder;
